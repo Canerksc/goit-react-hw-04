5cb13dbd60ffecc24668aee2c20b233018b54183
Caner <canerkesici08@gmail.com>
2025-08-13 22:52:47 +0300

    apı fix

--- src/API/unsplash.js
+++ src/API/unsplash.js
@@ -1,11 +1,14 @@
-const ACCESS_KEY = import.meta.env.VITE_UNSPLASH_ACCESS_KEY; 
+const API_KEY = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
 
 export async function fetchImages(query, page = 1) {
-  const API_KEY = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
+  console.log("Unsplash API Key:", API_KEY); 
+
   const url = `https://api.unsplash.com/search/photos?query=${query}&page=${page}&per_page=12&client_id=${API_KEY}`;
-
   const res = await fetch(url);
-  if (!res.ok) throw new Error("Veri alınırken hata oluştu");
 
-  return res.json(); // { results: [...], total_pages: number, ... }
+  if (!res.ok) {
+    throw new Error(`Veri alınırken hata oluştu: ${res.status}`);
+  }
+
+  return res.json();
 }
